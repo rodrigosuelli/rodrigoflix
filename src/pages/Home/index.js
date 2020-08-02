797fe82ab011dcfa7148133ed5d3373defdda53a
Rodrigo <rodrigosuellli@gmail.com>
2020-08-02 10:21:47 -0300

    Add day 2's code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
 
-function App() {
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+
+import dadosIniciais from '../../data/dados_iniciais.json';
+
+function Home() {
   return (
     <div style={{ background: '#141414' }}>
       <Menu />
@@ -26,9 +28,13 @@ function App() {
 
       <Carousel category={dadosIniciais.categorias[4]} />
 
+      <Carousel category={dadosIniciais.categorias[5]} />
+
+      <Carousel category={dadosIniciais.categorias[6]} />
+
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
